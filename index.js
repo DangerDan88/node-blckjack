ec3d0ce05404f429ed9135fed71df34b7b6e4643
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-11-07 18:25:13 -0700

    node blck jack base

---
+++ index.js
@@ -0,0 +1,7 @@
+// Black jack Node
+// 1. start game and deal out a value to yourself then deal out to computer
+//2. create a hit and stand function
+//3. Create a bust function along with win
+// node args start at 2 we skip 1.
+
+console.log(Ace);
