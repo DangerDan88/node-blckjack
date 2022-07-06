5b90f013240504b8fb2c4483eece6c369f8010c9
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-07-06 10:54:49 -0600

    working on skeleton for node black jack cli game

--- index.js
+++ index.js
@@ -4,4 +4,33 @@
 //3. Create a bust function along with win
 // node args start at 2 we skip 1.
 
-console.log(Ace);
+function homePage() {
+  console.log("-----------------------------");
+  console.log("      Welcome To BlackJack    ");
+  console.log("-----------------------------");
+}
+homePage();
+
+// this function will shuffle the deck or numbers for black jack
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10];
+shuffle(arr);
+//console.log(arr);
